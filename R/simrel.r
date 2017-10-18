ab701d408cfaa7c9f9fc09adc30d5a255eb7c913
Raju Rimal <therimalaya@gmail.com>
2017-10-18 15:02:04 +0200

    Validation, Test Data and Beta0 fixed

--- R/simrel.r
+++ R/simrel.r
@@ -51,6 +51,7 @@ Simrel <- R6::R6Class(
       rotation_y = NULL,
       beta_z     = NULL,
       beta       = NULL,
+      beta0      = NULL,
       Rsq_y      = NULL,
       Rsq_w      = NULL,
       minerror   = NULL
@@ -85,13 +86,18 @@ Simrel <- R6::R6Class(
       if(!is.list(relpos)) out <- unlist(out)
       return(out)
     },
-    ..get_data = function(n, p, sigma, rotation_x, m = 1, rotation_y = NULL){
+    ..get_data = function(n, p, sigma, rotation_x, m = 1, 
+                          rotation_y = NULL, muX = NULL, muY = NULL){
       sigma_rot <- chol(sigma)
       train_cal <- matrix(rnorm(n * (p + m), 0, 1), nrow = n) %*% sigma_rot
       Z <- train_cal[, (m + 1):(m + p), drop = F]
       X <- Z %*% t(rotation_x)
       W <- train_cal[, 1:m, drop = F]
       Y <- if (all(!is.null(m), m > 1)) W %*% t(rotation_y) else W
+      
+      if (!is.null(muX)) X <- sweep(X, 2, muX, "+")
+      if (!is.null(muY)) Y <- sweep(Y, 2, muY, "+")
+      
       list(y = unname(Y), x = X)
     }
   ),
@@ -145,15 +151,23 @@ Simrel <- R6::R6Class(
       params <- self$list_parameters
       list2env(params, envir = environment())
       switch(
-        type, 
+        type,
         univariate = {
           req_params <- c("n", "p", "q", "relpos", "R2", "gamma", "type")
-          ## ensure minimum required parameters
+          ## --- ensure minimum required parameters
           if (!all(req_params %in% names(params))) {
-            errors[[1]] <- "Not Enough Parameters"
+            stop("Not Enough Parameters to continue")
           }
           
-          ## ensure length params agree
+          ## --- Check muX and muY
+          if (exists("muX")) {
+            if (length(muX) != p) errors[[1]] <- "Length of muX is not equals to number of predictors (p)" 
+          }
+          if (exists("muY")) {
+            if (length(muY) != m) errors[[1]] <- "Length of muY is not equals to number of responses (m)" 
+          }
+          
+          ## --- ensure length params agree
           if (q <= length(relpos)) {
             errors[[2]] <- "Number of position of relevant components should be less than the number of relevant predictors."
           }
@@ -166,32 +180,76 @@ Simrel <- R6::R6Class(
           if (!all(relpos %in% 1:p)) {
             errors[[7]] <- paste("Position of relevant component must be between 1 and ", p)
           }
-          
-          ## Ensure range of parameters
+          ## --- Ensure range of parameters
           if (gamma < 0) {
             errors[[4]] <- "Gamma must be greater than zero."
           }
           if (!all(R2 > 0, R2 < 1)) {
             errors[[5]] <- "Coefficient of determination (R2) should lie between 0 and 1."
           }
-          
-          ## Ensure class of parameters
+          ## --- Ensure class of parameters
           if (!is.atomic(relpos)) {
             errors[[6]] <- "Relpos for uniresponse simulation should be a vector."
           }
         },
-        
         multivariate = {
-          browser()
-          ## Ensure minimum required parameters
-          req_params <- c("n", "p", "q", "relpos", "R2", 
+          ## --- Ensure minimum required parameters
+          req_params <- c("n", "p", "q", "relpos", "R2",
                           "gamma", "type", "ypos", "m")
-          if (!all(req_params %in% names(params))) {
-            errors[[1]] <- "Not Enough Parameters"
+          if (any(!req_params %in% names(params))) {
+            stop("Not Enough Parameters to continue")
+          }
+          
+          ## --- Check muX and muY
+          if (exists("muX")) {
+            if (length(muX) != p) errors[[1]] <- "Length of muX is not equals to number of predictors (p)" 
+          }
+          if (exists("muY")) {
+            if (length(muY) != m) errors[[1]] <- "Length of muY is not equals to number of responses (m)" 
+          }
+          
+          ## --- Ensure class of parameters
+          if (!is.list(relpos) | !is.list(ypos)) {
+            stop("relpos and ypos must be a list")
+          }
+          
+          if (!all(c(length(q), length(relpos), length(R2)) == length(q))) {
+            stop(paste("Length of q, relpos and R2 should be of same length"))
           }
           
           ## Ensure Lengths of parameters
-          ## START HERE -----------------
+          if (any(q < sapply(relpos, length))) {
+            errors[[2]] <- "Number of position of relevant components should be less than the number of relevant predictors for each response components."
+          }
+          if (sum(q) > p) {
+            errors[[3]] <- "Total number of relevant predictors should be less than or equals to total number of predictors."
+          }
+          if (!all(unlist(relpos) %in% 1:p)) {
+            errors[[4]] <- paste("Position of relevant predictor component of response must be between 1 and ", p)
+          }
+          if (any(duplicated(unlist(relpos)))) {
+            errors[[5]] <- "Relevant position of predictor components must be unique"
+          }
+          if (!all(unlist(ypos) %in% 1:m)) {
+            errors[[6]] <- paste("Position of response component must be between 1 and ", m)
+          }
+          if (any(duplicated(unlist(ypos)))) {
+            errors[[7]] <- "Position of response components must be unique"
+          }
+          if (length(unlist(ypos)) != m) {
+            errors[[8]] <- paste("Position of response components (ypos) must contain all response variabels from 1 to ", m)
+          }
+          ## --- Ensure range of parameters
+          if (gamma < 0) {
+            errors[[9]] <- "gamma must be greater than zero."
+          }
+          if (eta < 0) {
+            errors[[9]] <- "eta must be greater than zero."
+          }
+          if (any(sapply(R2, function(x) !all(x > 0, x < 1)))) {
+            errors[[10]] <- "Coefficients of determination (R2) should lie between 0 and 1."
+          }
+          
         }
       )
       out <- list(errors = errors, warnings = warnings)
@@ -214,15 +272,34 @@ Simrel <- R6::R6Class(
     set_properties = function(key, expression) {
       private$..properties[[key]] <- eval(expression)
     },
-    get_data = function(){
+    get_data = function(type = c("train", "test", "both")){
+      type <- match.arg(type)
       n <- self$get_parameters("n")
       p <- self$get_parameters("p")
       m <- self$get_parameters("m")
       sigma <- self$get_properties("sigma")
       rotation_x <- self$get_properties("rotation_x")
       rotation_y <- self$get_properties("rotation_y")
-      data <- private$..get_data(n, p, sigma, rotation_x, m, rotation_y)
-      data.frame(y = I(data$y), x = I(data$x))
+      muX <- self$get_parameters("muX")
+      muY <- self$get_parameters("muY")
+      
+      train <- private$..get_data(n, p, sigma, rotation_x, m, rotation_y, muX, muY)
+      out <- data.frame(y = I(train$y), x = I(train$x))
+      if (type %in% c("test", "both")) {
+        ntest <- self$get_parameters("ntest")
+        if(is.null(ntest)){
+          warning("ntest is not supplied, so n is used for test data")
+          ntest <- n
+        }
+        test <- private$..get_data(ntest, p, sigma, rotation_x, m, rotation_y, muX, muY)
+        out <- list(
+          train = data.frame(y = I(train$y), x = I(train$x)),
+          test = data.frame(y = I(test$y), x = I(test$x))
+        )
+        return(out)
+      }
+      
+      return(out)
     }
   )
 )
@@ -254,13 +331,13 @@ UniSimrel <- R6::R6Class(
     initialize = function(...){
       super$initialize(...)
       super$set_parameters(type = "univariate")
-      
+
       ## Validate the parameters
       is_valid <- self$get_validated()
       is_valid <- lapply(is_valid, function(x) x[!sapply(x, is.null)])
       if (length(is_valid$errors)) stop(unlist(is_valid$errors))
       if (length(is_valid$warnings)) warning(unlist(is_valid$warnings))
-      
+
       ## Adding Properties to Simrel Object
       super$base_properties()
       self$add_properties()
@@ -301,6 +378,15 @@ UniSimrel <- R6::R6Class(
         beta_z <- self$get_properties("beta_z")
         return(rotation_x %*% beta_z)
       }))
+      self$set_properties("beta0", expression({
+        beta0 <- rep(0, self$get_parameters("m"))
+        muX <- self$get_parameters("muX")
+        muY <- self$get_parameters("muY")
+        if (!is.null(muX)) beta <- self$get_properties("beta")
+        if (!is.null(muY)) beta0 <- beta0 + muY
+        if (!is.null(muX)) beta0 <- beta0 - t(beta) %*% muX
+        return(c(beta0))
+      }))
       self$set_properties("Rsq_y", expression({
         beta_z <- self$get_properties("beta_z")
         sigma_zy <- self$get_properties("sigma_zy")
@@ -339,26 +425,24 @@ MultiSimrel <- R6::R6Class(
   inherit = Simrel,
   public = list(
     initialize = function(...){
-      if(missing(...)) {
-        super$initialize(
-          q = c(6, 7),
-          m = 3,
-          relpos = list(c(1, 2), c(3, 4, 5)),
-          ypos = list(1, c(2, 3)),
-          R2 = c(0.7, 0.9),
-          type = "multivariate"
-        )
-      } else {
-        super$initialize(...)
-        super$set_parameters(type = "multivariate")
-      }
-      
+      params <- list(...)
+      default_params <- list(
+        q = c(6, 7),
+        m = 3,
+        relpos = list(c(1, 2), c(3, 4, 5)),
+        ypos = list(1, c(2, 3)),
+        R2 = c(0.7, 0.9),
+        type = "multivariate"
+      )
+      default_params[names(params)] <- params
+      do.call(super$initialize, default_params)
+
       ## Validate the parameters
       is_valid <- self$get_validated()
       is_valid <- lapply(is_valid, function(x) x[!sapply(x, is.null)])
       if (length(is_valid$errors)) stop(unlist(is_valid$errors))
       if (length(is_valid$warnings)) warning(unlist(is_valid$warnings))
-      
+
       ## Adding Properties to Simrel Object
       super$base_properties()
       self$add_properties()
@@ -377,7 +461,7 @@ MultiSimrel <- R6::R6Class(
         p <- self$get_parameters("p")
         lambda <- self$get_properties("eigen_x")
         eta <- self$get_properties("eigen_w")
-        out <- mapply(private$..get_cov, pos = relpos, Rsq = R2, eta = eta, 
+        out <- mapply(private$..get_cov, pos = relpos, Rsq = R2, eta = eta,
                       MoreArgs = list(p = p, lambda = lambda))
         cbind(out, rep(0, m - length(eta)))
       }))
@@ -436,6 +520,15 @@ MultiSimrel <- R6::R6Class(
         rotation_y <- self$get_properties("rotation_y")
         beta_z <- self$get_properties("beta_z")
         return(rotation_x %*% beta_z %*% t(rotation_y))
+      }))
+      self$set_properties("beta0", expression({
+        beta0 <- rep(0, self$get_parameters("m"))
+        muX <- self$get_parameters("muX")
+        muY <- self$get_parameters("muY")
+        if (!is.null(muX)) beta <- self$get_properties("beta")
+        if (!is.null(muY)) beta0 <- beta0 + muY
+        if (!is.null(muX)) beta0 <- beta0 - t(beta) %*% muX
+        return(c(beta0))
       }))
       self$set_properties("Rsq_w", expression({
         beta_z <- self$get_properties("beta_z")
